83e143e9ec0867233b9df82595441a254920c5af
JackUCC <122489574@umail.ucc.ie>
2024-11-08 12:51:52 +0000

    secound commit

--- mymath.py
+++ mymath.py
@@ -1,19 +1,32 @@
 def add_three(a, b, c):
-    return -1
+    return a + b + c
 
 def subtract_three(a, b, c):
-    return -1
+    return a - b - c
 
 def multiply_three(a, b, c):
-    return -1
+    return a * b * c
 
 def divide_three(a, b, c):
-    return -1
+    return a / b / c
 
 def square(a):
     #return a**2
-    return -1
+    return a**2
+
+def square(b):
+    #return a**2
+    return b**2
+def square(c):
+    #return a**2
+    return c**2
 
 def cube(a):
     #return a**3
-    return -1
+    return a**3
+def cube(b):
+    #return a**3
+    return b**3
+def cube(c):
+    #return a**3
+    return c**3
